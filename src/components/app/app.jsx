2aee3c7b9fe1d680967eda058409fda268a82f2c
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-06-16 10:18:56 +0600

    Добавляет PropTypes в компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import WelcomeScreen from './../welcome-screen/welcome-screen.jsx';
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorCount} = props;
 
   return <
     WelcomeScreen
     errorCount = {errorCount}
   />;
+};
+
+App.propTypes = {
+  errorCount: PropTypes.number.isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
 const init = () => {
-  const settings = {
-    errorCount: 3
+  const Settings = {
+    ERROR_COUNT: 3
   };
 
   ReactDOM.render(
       <App
-        errorCount = {settings.errorCount ? settings.errorCount : 1}
+        errorCount = {Settings.ERROR_COUNT}
       />,
       document.querySelector(`#root`)
   );
