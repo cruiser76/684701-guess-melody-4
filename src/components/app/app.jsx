3cef55189d0e2deab5ab221b631c7048558b0ffe
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-06-21 23:38:02 +0600

    Добавляет роутер и подготовленную разметку для экранов вопросов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import WelcomeScreen from './../welcome-screen/welcome-screen.jsx';
+import QuestionArtist from './../question-artist/question-artist.jsx';
+import QuestionGenre from './../question-genre/question-genre.jsx';
 
 const onWelcomeButtonHandler = () => {
   return;
@@ -11,11 +14,23 @@ const App = (props) => {
 
   const {errorCount} = props;
 
-  return <
-    WelcomeScreen
-    errorCount={errorCount}
-    onWelcomeButtonClick={onWelcomeButtonHandler}
-  />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorCount={errorCount}
+            onWelcomeButtonClick={onWelcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtist />
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenre />
+        </Route>
+      </Switch>
+    </BrowserRouter>);
 };
 
 App.propTypes = {
