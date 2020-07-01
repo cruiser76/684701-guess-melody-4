2d8b063ef25fdeb7f601ed1a8810ac519c2f8189
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-07-01 08:15:13 +0600

    Исправляет тест е2е

--- src/components/audio-player/audio-player.e2e.test.js
+++ src/components/audio-player/audio-player.e2e.test.js
@@ -8,28 +8,27 @@ configure({adapter: new Adapter()});
 
 it(`Click by Play button toggle className`, () => {
   const handlePlayButtonClick = jest.fn();
-  jest.spyOn(window.HTMLMediaElement.prototype, `pause`)
-    .mockImplementation(() => {});
   const screen = mount(
       <AudioPlayer
-        isPlaying={false}
+        isPlaying={true}
         src={``}
         onPlayButtonClick={handlePlayButtonClick}
       />
   );
+  window.HTMLMediaElement.prototype.play = () => {};
+  window.HTMLMediaElement.prototype.pause = () => {};
 
   const btnPlay = screen.find(`.track__button`);
-  expect(btnPlay.hasClass(`track__button--play`)).toBe(true);
+  expect(btnPlay).toHaveLength(1);
+
   btnPlay.props().onClick();
-  // btnPlay.simulate(`click`);
-  expect(btnPlay.hasClass(`track__button--play`)).toBe(true);
+  btnPlay.simulate(`click`);
+  expect(handlePlayButtonClick).toHaveBeenCalledTimes(1);
+  expect(screen.state().isPlaying).toBe(false);
+  btnPlay.simulate(`click`);
 
-
-  // expect(handlePlayButtonClick).toHaveBeenCalledTimes(1);
-
-  // const btnPause = screen.find(`.track__button`);
-  // btnPlay.props().onClick();
-  // btnPlay.simulate(`click`);
-  // expect(btnPlay.hasClass(`track__button--play`)).toBe(true);
-  // expect(handlePlayButtonClick).toHaveBeenCalledTimes(1);
+  const btnPause = screen.find(`.track__button`);
+  btnPause.props().onClick();
+  expect(screen.state().isPlaying).toBe(true);
+  expect(handlePlayButtonClick).toHaveBeenCalledTimes(2);
 });
